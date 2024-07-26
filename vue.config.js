4838608d7f3b7ce036d95e0e0e6e230373f1e86e
aht9056 <s1091872@gm.pu.edu.tw>
2024-07-26 11:00:44 +0800

    gh-pages 測試

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave:false,
-  publicPath: '/githubTest/dist/'
+    transpileDependencies: true,
+    lintOnSave: false,
+    publicPath: process.env.NODE_ENV === 'production' ? '/githubTest/' : '/',
 })
